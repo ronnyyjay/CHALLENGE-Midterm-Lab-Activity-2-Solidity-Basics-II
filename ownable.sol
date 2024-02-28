5c192c402cc23398f41a197e0e2564d73a20de4b
AaronRamales <99706675+ronnyyjay@users.noreply.github.com>
2024-02-28 22:23:19 +0800

    Update ownable.sol

--- ownable.sol
+++ ownable.sol
@@ -1,46 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.3;
+pragma solidity ^0.8.0;
 
-contract SalaryCalculator {
+contract Ownable {
     address public owner;
-    uint256 public age;
-    uint256 public hoursWorked;
-    uint256 public hourlyRate;
-    uint256 public calculateTotSal;
+    
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
     
     constructor() {
         owner = msg.sender;
     }
     
     modifier onlyOwner() {
-        require(msg.sender == owner, "Only contract owner can call this function");
+        require(msg.sender == owner, "Only the owner can call this function");
         _;
     }
     
-    modifier validRate(uint256 _rate) {
-        require(_rate > 0, "Hourly rate must be greater than zero");
-        _;
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "New owner address cannot be zero");
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
     }
+}
+
+contract MyContract is Ownable {
+    uint256 public someValue;
     
-    modifier validHoursWorked(uint256 _hours) {
-        require(_hours > 0, "Hours worked must be greater than zero");
-        _;
-    }
-    
-    function setAge(uint256 _age) public {
-        age = _age;
-    }
-    
-    function setHrsWorked(uint256 _hours) public validHoursWorked(_hours) {
-        hoursWorked = _hours;
-    }
-    
-    function setRate(uint256 _rate) public onlyOwner validRate(_rate) {
-        hourlyRate = _rate;
-    }
-    
-    function calculateTotalSalary() public onlyOwner {
-        require(hourlyRate > 0 && hoursWorked > 0, "Hourly rate or hours worked is zero");
-        calculateTotSal = hoursWorked * hourlyRate;
+    function setSomeValue(uint256 newValue) public onlyOwner {
+        someValue = newValue;
     }
 }
